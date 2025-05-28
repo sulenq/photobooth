233f761a60fe4b4d311ed31119fbc1ca0c4a914d
sulenq <fatwalinovera@gmail.com>
2025-05-28 09:47:35 +0700

    styling

--- src/components/widget/PageContainer.tsx
+++ src/components/widget/PageContainer.tsx
@@ -4,7 +4,12 @@ import CContainer from "../ui-custom/CContainer";
 interface Props extends StackProps {}
 const PageContainer = ({ children, ...restProps }: Props) => {
   return (
-    <CContainer p={10} minH={"100dvh"} bg={"p.100"}>
+    <CContainer
+      onContextMenu={(e) => e.preventDefault()}
+      p={10}
+      minH={"100dvh"}
+      bg={"p.100"}
+    >
       <CContainer
         p={10}
         border={"1px solid {colors.p.900}"}
