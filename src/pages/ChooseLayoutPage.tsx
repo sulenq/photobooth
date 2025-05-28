3336fcc348b0194931bd1e07d48799a20030cec3
sulenq <fatwalinovera@gmail.com>
2025-05-28 13:33:01 +0700

    styling

--- src/pages/ChooseLayoutPage.tsx
+++ src/pages/ChooseLayoutPage.tsx
@@ -7,9 +7,114 @@ import { LAYOUT_OPTIONS } from "@/constants/layoutOptions";
 import useSessionTemplate from "@/context/useSessionTemplate";
 import { Box, HStack, Icon, Image } from "@chakra-ui/react";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+import { useRef } from "react";
+
+const OptionList = () => {
+  // Contexts
+  const setTemplate = useSessionTemplate((s) => s.setTemplate);
+
+  // Refs
+  const carouselContainerRef = useRef<HTMLDivElement>(null);
+
+  // Utils
+  function prev() {
+    if (carouselContainerRef.current) {
+      carouselContainerRef.current.scrollLeft -= 300;
+    }
+  }
+  function next() {
+    if (carouselContainerRef.current) {
+      carouselContainerRef.current.scrollLeft += 300;
+    }
+  }
+
+  return (
+    <Box flexShrink={0} h={"50%"} bg={"p.900"} p={10}>
+      <Heading1
+        className="df"
+        color={"p.100"}
+        fontWeight={"semibold"}
+        textAlign={"center"}
+        mb={8}
+      >
+        Choose a Template
+      </Heading1>
+
+      <Box pos={"relative"} h={"calc(100% - 64px - 20px)"}>
+        {/* Controls */}
+        <HStack
+          position={"absolute"}
+          top={"50%"}
+          transform={"translateY(-50%)"}
+          w={"full"}
+          justify={"space-between"}
+          zIndex={2}
+          pointerEvents={"none"}
+        >
+          <BButton
+            iconButton
+            pointerEvents={"auto"}
+            w={"fit"}
+            borderRadius={"full"}
+            bg={"white"}
+            size={"xl"}
+            ml={-4}
+            onClick={prev}
+          >
+            <Icon color={"p.900"} boxSize={6}>
+              <IconChevronLeft stroke={3} />
+            </Icon>
+          </BButton>
+
+          <BButton
+            iconButton
+            pointerEvents={"auto"}
+            w={"fit"}
+            borderRadius={"full"}
+            bg={"white"}
+            size={"xl"}
+            mr={-4}
+            onClick={next}
+          >
+            <Icon color={"p.900"} boxSize={6}>
+              <IconChevronRight stroke={3} />
+            </Icon>
+          </BButton>
+        </HStack>
+
+        {/* Option List */}
+        <Box
+          ref={carouselContainerRef}
+          scrollBehavior={"smooth"}
+          h={"full"}
+          overflowX={"auto"}
+          className="noScroll"
+        >
+          <HStack h={"full"} w={"max"} align={"stretch"} gap={8}>
+            {LAYOUT_OPTIONS.map((item, i) => {
+              return (
+                <CContainer
+                  key={i}
+                  h={"full"}
+                  w={"fit"}
+                  cursor={"pointer"}
+                  className="clicky"
+                  onClick={() => setTemplate(item)}
+                >
+                  <Image src={item?.thumbnail} h={"full"} />
+                </CContainer>
+              );
+            })}
+          </HStack>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 const ChooseLayoutPage = () => {
-  const { template, setTemplate } = useSessionTemplate();
+  // Contexts
+  const template = useSessionTemplate((s) => s.template);
 
   return (
     <Box h={"100dvh"}>
@@ -25,78 +130,7 @@ const ChooseLayoutPage = () => {
         </HStack>
       </Box>
 
-      <Box flexShrink={0} h={"50%"} bg={"p.900"} p={10}>
-        <Heading1
-          className="df"
-          color={"p.100"}
-          fontWeight={"semibold"}
-          textAlign={"center"}
-          mb={8}
-        >
-          Choose a Template
-        </Heading1>
-
-        <Box pos={"relative"} h={"calc(100% - 64px - 20px)"}>
-          {/* Controls */}
-          <HStack
-            position={"absolute"}
-            top={"50%"}
-            transform={"translateY(-50%)"}
-            w={"full"}
-            justify={"space-between"}
-            zIndex={2}
-            pointerEvents={"none"}
-          >
-            <BButton
-              iconButton
-              pointerEvents={"auto"}
-              w={"fit"}
-              borderRadius={"full"}
-              bg={"white"}
-              size={"xl"}
-              ml={-4}
-            >
-              <Icon color={"p.900"} boxSize={6}>
-                <IconChevronLeft stroke={3} />
-              </Icon>
-            </BButton>
-
-            <BButton
-              iconButton
-              pointerEvents={"auto"}
-              w={"fit"}
-              borderRadius={"full"}
-              bg={"white"}
-              size={"xl"}
-              mr={-4}
-            >
-              <Icon color={"p.900"} boxSize={6}>
-                <IconChevronRight stroke={3} />
-              </Icon>
-            </BButton>
-          </HStack>
-
-          {/* Option List */}
-          <Box h={"full"} overflowX={"auto"} className="noScroll">
-            <HStack h={"full"} w={"max"} align={"stretch"} gap={8}>
-              {LAYOUT_OPTIONS.map((item, i) => {
-                return (
-                  <CContainer
-                    key={i}
-                    h={"full"}
-                    w={"fit"}
-                    cursor={"pointer"}
-                    className="clicky"
-                    onClick={() => setTemplate(item)}
-                  >
-                    <Image src={item?.thumbnail} h={"full"} />
-                  </CContainer>
-                );
-              })}
-            </HStack>
-          </Box>
-        </Box>
-      </Box>
+      <OptionList />
     </Box>
   );
 };
