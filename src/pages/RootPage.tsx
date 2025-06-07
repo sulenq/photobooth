ad3038e6108e596a4fb9d5d0f8ac64f2d34b919d
sulenq <fatwalinovera@gmail.com>
2025-06-07 17:55:48 +0700

    refactor

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -26,7 +26,7 @@ const RootPage = () => {
 
       <Image src={`${IMAGES_PATH}/logo_dark.png`} />
 
-      <NavLink to="/print-send" align={"center"}>
+      <NavLink to="/choose-product" align={"center"}>
         <BButton mt={10} {...PRESET_MAIN_BUTTON}>
           START PHOTO
         </BButton>
